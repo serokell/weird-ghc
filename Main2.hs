2f1eacc77463ba41d07e7009b3134fbf61290f2c
Kirill Elagin <kirelagin@gmail.com>
2019-04-29 12:07:16 +0200

    Simplify Main2

--- Main2.hs
+++ Main2.hs
@@ -4,21 +4,10 @@ module Main
   ( main
   ) where
 
-import KValue (KValue (KvInt), TKind, ToTK, toKVal)
-
-
-data Value' t where
-  VC :: KValue t -> Value' t
-
-
-class IsoValue a where
-  toVal :: a -> Value' (ToTK Integer)
-
-instance IsoValue Integer where
-  toVal = VC . toKVal
+import KValue (KValue (KvInt), toKVal)
 
 
 main :: IO ()
 main = do
-  let (VC (KvInt i)) = toVal (41 :: Integer)
+  let (KvInt i) = (id . toKVal) (41 :: Integer)
   print i
